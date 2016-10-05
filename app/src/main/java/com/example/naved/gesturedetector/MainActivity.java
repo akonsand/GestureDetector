a2045f892dbdf36da33a25771c1bb293658b96d7
Naved Merchant <naved.merchant@gmail.com>
2016-10-05 19:20:11 -0400

    initial commit

---
+++ app/src/main/java/com/example/naved/gesturedetector/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.naved.gesturedetector;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManager;
+    Sensor acc;
+    Sensor rotation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        acc = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        rotation = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        sensorManager.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, rotation, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this, acc);
+        sensorManager.unregisterListener(this, rotation);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            Log.d("ACC", "event x " + event.values[0] + "event y" + event.values[1] + "event z" + event.values[2]);
+        } else
+        {
+            //Log.d("ROTATION", "event x " + event.values[0] + "event y" + event.values[1] + "event z" + event.values[2] + "scalar " + event.values[3]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
