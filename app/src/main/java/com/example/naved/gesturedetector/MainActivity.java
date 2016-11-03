3e2cbe9f333d7b1942bb5a76fe97c29c440b327b
Archana Krishnan <ak232@buffalo.edu>
2016-11-03 14:18:26 -0400

    4 gestures added. Proximity and accelerometer based.

--- app/src/main/java/com/example/naved/gesturedetector/MainActivity.java
+++ app/src/main/java/com/example/naved/gesturedetector/MainActivity.java
@@ -5,25 +5,24 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    static final int SHAKE_INTERVAL = 3000;
-    static final int NUMBER_OF_ACTIONS = 2;
-    static final int VOLUME_UP_THRESHOLD = 4;
-    static final int VOLUME_DOWN_THRESHOLD = -4;
-    static final int RECENT_APPS_THRESHOLD = 4;
-    static final int HOME_THRESHOLD = 4;
 
     SensorManager sensorManager;
+    Sensor wave;
     Sensor shake;
+    Sensor acc;
     TextView screen;
+    Boolean doubleWave = false;
+    Boolean waiting = false;
 
-    long prevActionTime;
-    int volumeUpActionCount, volumeDownActionCount, recentAppsActionCount, homeActionCount = 0;
+    long proxyPrevActionTime;
+
     final String TAG = "MainActivity";
 
     @Override
@@ -31,86 +30,66 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        shake = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
+        wave = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        shake = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        sensorManager.registerListener(this, wave, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this, shake, SensorManager.SENSOR_DELAY_NORMAL);
 
         screen = (TextView) findViewById(R.id.yolo);
-        prevActionTime = System.currentTimeMillis();
+        proxyPrevActionTime = System.currentTimeMillis();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        sensorManager.unregisterListener(this, wave);
         sensorManager.unregisterListener(this, shake);
+        sensorManager.unregisterListener(this, acc);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
-        float currentx = (int) Math.round(event.values[0]);
-        float currenty = (int) Math.round(event.values[1]);
-        float currentz = (int) Math.round(event.values[2]);
-
-        long curTime = System.currentTimeMillis();
-
-        long timeElapsed = curTime - this.prevActionTime;
-
-        if(currentx >= VOLUME_UP_THRESHOLD) {
-            Log.d(TAG,"UP");
-            volumeUpActionCount++;
-            this.prevActionTime = curTime;
-            if(timeElapsed < SHAKE_INTERVAL) {
-               if(volumeUpActionCount >= NUMBER_OF_ACTIONS) {
-                    Log.d(TAG, "Volume up");
-                    screen.setText("Volume up");
-                    volumeUpActionCount = 0;
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            if(event.values[0] < 10) {
+                long currentTime = System.currentTimeMillis();
+                //Log.d(TAG, event.values[0] + "");
+                if(!waiting) {
+                    waiting = true;
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(!doubleWave) {
+                                screen.setText("Single wave");
+                            }
+                            doubleWave = false;
+                            waiting = false;
+                        }
+                    }, 1000);
+                } else {
+                    long timeSinceLastWave =  currentTime - proxyPrevActionTime;
+                    Log.d(TAG, timeSinceLastWave + "");
+                    if(timeSinceLastWave < 2000) {
+                        doubleWave = true;
+                        screen.setText("Double wave");
+                    }
                 }
-            } else {
-                volumeUpActionCount = 0;
+                proxyPrevActionTime = currentTime;
             }
-        } else if(currentx <= VOLUME_DOWN_THRESHOLD) {
-            Log.d(TAG,"DOWN");
-            volumeDownActionCount++;
-            this.prevActionTime = curTime;
-            if(timeElapsed < SHAKE_INTERVAL) {
-                if(volumeDownActionCount >= NUMBER_OF_ACTIONS) {
-                    Log.d(TAG, "Volume down");
-                    screen.setText("Volume down");
-                    volumeDownActionCount = 0;
+        }
+        else if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER ) {
+            if(event.values[0] > 25) {
+                if(event.values[1] > 10) {
+                    screen.setText("Double twist");
+                } else {
+                    screen.setText("Chop chop");
                 }
-            } else  {
-                volumeDownActionCount =0;
-            }
-        } else if(currentz > RECENT_APPS_THRESHOLD) {
-            recentAppsActionCount++;
-            this.prevActionTime = curTime;
-            if(timeElapsed < SHAKE_INTERVAL) {
-                if(recentAppsActionCount >= NUMBER_OF_ACTIONS) {
-                    Log.d(TAG, "Recent apps");
-                    screen.setText("Recent apps");
-                    recentAppsActionCount = 0;
-                }
-            } else {
-                recentAppsActionCount = 0;
-            }
-        } else if(currenty > HOME_THRESHOLD) {
-            homeActionCount++;
-            this.prevActionTime = curTime;
-            if(timeElapsed < SHAKE_INTERVAL) {
-                if(homeActionCount >= NUMBER_OF_ACTIONS) {
-                    Log.d(TAG, "Home");
-                    screen.setText("Home");
-                    homeActionCount = 0;
-                }
-            } else {
-                homeActionCount = 0;
             }
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 }
